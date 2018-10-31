54ceb9d6932696b52d2b079a59dcc2a1c99880b2
geethree <gerald@3scan.com>
2018-10-31 12:17:56 -0700

    Use npm to generate css prior to generating the website with hugo

--- press_site.sh
+++ press_site.sh
@@ -1,9 +1,19 @@
- #!/bin/bash
+#!/bin/bash
+
+# NOTE(G3): Ensure that the css is generated
+npm install
+npm run build
+
+# NOTE(G3): Generate the website
 hugo
-git status
+
+# NOTE(G3): Ensure all content and generated docs are 
+# in source control.
 git add content/ docs/
-read -p "Enter commit message:" commit
+git status
+read -p "Enter commit message: " commit
 git commit -am "$commit"
+
 read -p "Push to master [y|n]?" push
 case "$push" in
 	y|Y ) git push origin master;;
